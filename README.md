73907ef8f3b1596b2353ea4cd9f33532dee4224b
Erik Hollensbe <git@hollensbe.org>
2023-05-17 23:27:44 -0700

    Initial commit
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

---
+++ README.md
@@ -0,0 +1,8 @@
+sterno is a simple daemon to fake prometheus metrics. You can use the
+[example_sterno.conf](example_sterno.conf) to get an idea of how to use it. It
+has `-help`.
+
+More to come here, but I'm tired.
+
+Author: <erik@hollensbe.org>
+License: MIT
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Port      uint          `yaml:"port"`
+	Namespace string        `yaml:"namespace"`
+	Metrics   []Metric      `yaml:"metrics"`
+	Interval  time.Duration `yaml:"interval"`
+}
+
+type Metric struct {
+	Name        string  `yaml:"name"`
+	Subsystem   string  `yaml:"subsystem"`
+	Type        string  `yaml:"type"`
+	Random      bool    `yaml:"random"`
+	StaticValue float64 `yaml:"static_value"`
+	RandomMax   uint64  `yaml:"random_max"`
+}
+
+func buildMetrics(c Config) (*prometheus.Registry, error) {
+	reg := prometheus.NewRegistry()
+
+	for _, metric := range c.Metrics {
+		if metric.Subsystem == "" {
+			metric.Subsystem = "sterno"
+		}
+
+		switch metric.Type {
+		case "gauge":
+			gauge := prometheus.NewGauge(prometheus.GaugeOpts{
+				Namespace: c.Namespace,
+				Subsystem: metric.Subsystem,
+				Name:      metric.Name,
+			})
+
+			reg.MustRegister(gauge)
+
+			go func(gauge prometheus.Gauge, c Config, metric Metric) {
+				for {
+					if metric.Random {
+						buf := make([]byte, 8)
+						if _, err := rand.Reader.Read(buf); err != nil {
+							log.Printf("Error while reading random data: %v", err)
+							continue
+						}
+
+						res, err := binary.Uvarint(buf)
+						if err < 0 {
+							log.Printf("Error while unpacking uvarint: %v", err)
+						}
+
+						gauge.Set(float64(res%metric.RandomMax + 1))
+					} else {
+						gauge.Set(metric.StaticValue)
+					}
+
+					time.Sleep(c.Interval)
+				}
+			}(gauge, c, metric)
+
+		default:
+			return nil, fmt.Errorf("Type %q is not supported", metric.Type)
+		}
+	}
+
+	return reg, nil
+}
+
+func main() {
+	var (
+		config = flag.String("config", "sterno.conf", "Path to sterno configuration file; YAML or JSON")
+	)
+
+	flag.Parse()
+
+	out, err := os.ReadFile(*config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var c Config
+
+	if err := yaml.Unmarshal(out, &c); err != nil {
+		log.Fatal(err)
+	}
+
+	if c.Namespace == "" {
+		c.Namespace = "sterno"
+	}
+
+	if c.Interval == 0 {
+		c.Interval = time.Second
+	}
+
+	reg, err := buildMetrics(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.Handle("/metrics", promhttp.HandlerFor(
+		reg,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+			// Pass custom registry
+			Registry: reg,
+		},
+	))
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil))
+}
